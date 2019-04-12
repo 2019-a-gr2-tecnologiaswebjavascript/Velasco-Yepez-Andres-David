23a33db79b786df3bf23ad5f3bd43a39966f3e3f
Andrés Velasco <velasco.andrs@gmail.com>
2019-04-12 17:59:14 -0500

    directivas-eventos-propiedas-y algo mas

--- 01-Angular/galeria/src/app/app.component.ts
+++ 01-Angular/galeria/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'galeria';
+  clase = "rojo";
+
+  arregloFloresJS = [
+    {'nombre':"papitas", 'descripcion':"a lo bestia"},
+    {'nombre':"carnitas", 'descripcion':"a lo bestia"},
+    {'nombre':"chicas", 'descripcion':"a lo bestia"},
+  ];
+
+  cambioCalvo(evento:boolean){
+    // Logica para hacerle rojo
+    this.clase = "rojo";
+    console.log("Cambio a calvo ",evento);
+  }
+  cambioHomero(evento:boolean){
+    // Logica para hacerle amarillo
+    this.clase = "amarillo";
+    console.log("Cambio a homero ",evento);
+  }
+
+}
+
+class Flor {
+  // public ayuda a no tener que poner dos veces la variable
+  constructor(public nombre:string,public descripcion:string){
+
+  }
 }
